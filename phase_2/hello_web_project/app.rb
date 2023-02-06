2847c2fc7a67379bded96d1b422fea6d73df8d22
Abigail <ajmakovsky@gmail.com>
2023-02-06 17:22:00 +0000

    Project Set Up

---
+++ phase_2/hello_web_project/app.rb
@@ -0,0 +1,48 @@
+require 'sinatra/base'
+require 'sinatra/reloader'
+
+class Application < Sinatra::Base
+# This allows the app code to refresh
+  # without having to restart the server.
+  configure :development do
+    register Sinatra::Reloader
+  end
+
+  get '/names' do 
+
+    name1 = params[:name1]
+    name2 = params[:name2]
+    name3 = params[:name3]
+
+
+    return "#{name1}, #{name2}, #{name3}"
+  end
+
+  post '/sort-names' do 
+
+    names = params[:names]
+    split_names = names.split(",")
+    sorted_names = split_names.sort.join(",")
+
+    return sorted_names
+  end
+
+
+ 
+end 
+
+ 
+
+
+
+
+  # get '/hello' do 
+  #   name = params[:name]
+  #   return "Hello #{name}"
+  # end 
+
+  # post '/submit' do 
+  #   name = params[:name]
+  #   message = params[:message]
+  #   return "Thanks #{name}, you sent this message: #{message}"
+  # end 
---
+++ phase_2/hello_web_project/spec/integration/app_spec.rb
@@ -0,0 +1,67 @@
+require 'spec_helper'
+require 'rack/test'
+require_relative '../../app'
+
+describe Application do 
+
+  # so we can use rack-test helper methods
+  include Rack::Test::Methods
+
+  let(:app) { Application.new }
+
+  context "GET /" do
+    it 'returns Julia, Mary, Karim' do
+      # Assuming the post with id 1 exists.
+      response = get('/names?name1=Julia&name2=Mary&name3=Karim')
+
+      expect(response.status).to eq(200)
+      expect(response.body).to eq "Julia, Mary, Karim"
+    end
+
+    it 'returns 404 Not Found' do
+      response = get('/name')
+
+      expect(response.status).to eq(404)
+      # expect(response.body).to eq(expected_response)
+    end
+  end
+
+  context "POST /" do 
+    it 'returns sorted string: Alice,Joe,Julia,Kieran,Zoe' do 
+      response = post('/sort-names?names=Joe,Alice,Zoe,Julia,Kieran')
+
+      expect(response.status).to eq(200)
+      expect(response.body).to eq "Alice,Joe,Julia,Kieran,Zoe"
+    end 
+
+    it 'returns sorted string: Abi,Greg,Larry,Maisie,Ollie' do 
+      response = post('/sort-names?names=Greg,Abi,Ollie,Maisie,Larry')
+
+      expect(response.status).to eq(200)
+      expect(response.body).to eq "Abi,Greg,Larry,Maisie,Ollie"
+    end 
+  end
+end
+
+# # Request:
+
+# POST /sort-names?names"Joe,Alice,Zoe,Julia,Kieran"
+
+# # Expected response:
+
+# Response for 200 OK
+
+# "Alice,Joe,Julia,Kieran,Zoe"
+
+
+# # Request:
+
+# POST /sort-names?names"Greg,Abi,Ollie,Maisie,Larry"
+
+# # Expected response:
+
+# Response for 200 OK 
+
+# "Abi,Greg,Larry,Maisie,Ollie"
+
+ 
