7e09413ef2fb004f3a39240a4d858c5f672ab686
Abigail <ajmakovsky@gmail.com>
2023-02-07 15:59:21 +0000

    Removed Cloned Directory

---
+++ phase_2/saved_files/code.rb
@@ -0,0 +1,400 @@
+# file: app.rb
+require 'sinatra'
+require "sinatra/reloader"
+require_relative 'lib/database_connection'
+require_relative 'lib/album_repository'
+require_relative 'lib/artist_repository'
+
+DatabaseConnection.connect
+
+class Application < Sinatra::Base
+  configure :development do
+    register Sinatra::Reloader
+    also_reload 'lib/album_repository'
+    also_reload 'lib/artist_repository'
+  end
+
+  get '/albums' do 
+    repo = AlbumRepository.new
+    albums = repo.all 
+
+    response = albums.map do |album|
+      album.title
+    end.join(", ")
+
+    return response
+  end 
+
+  post '/albums' do 
+    repo = AlbumRepository.new
+    new_album = Album.new
+    new_album.title = params[:title]
+    new_album.release_year = params[:release_year]
+    new_album.artist_id = params[:artist_id]
+
+    repo.create(new_album)
+
+    return ''
+  end
+
+  get '/artists' do 
+    repo = ArtistRepository.new 
+
+    artists = repo.all
+
+    response = artists.map do |artist_name|
+      artist_name.name
+    end.join(", ")
+
+    return response
+  end 
+
+  post '/artists' do 
+    repo = ArtistRepository.new 
+
+    new_artist = Artist.new
+    new_artist.name = params[:name]
+    new_artist.genre = params[:name]
+
+    repo.create(new_artist)
+
+    return ''
+  end 
+end
+
+
+# {{ POST }} {{ /artists }} Route Design Recipe
+
+_Copy this design recipe template to test-drive a Sinatra route._
+
+## 1. Design the Route Signature
+
+You'll need to include:
+  * the HTTP method
+  * the path
+  * any query parameters (passed in the URL)
+  * or body parameters (passed in the request body)
+
+  method: POST 
+  path: /artists
+
+## 2. Design the Response
+
+The route might return different responses, depending on the result.
+
+For example, a route for a specific blog post (by its ID) might return `200 OK` if the post exists, but `404 Not Found` if the post is not found in the database.
+
+Your response might return plain text, JSON, or HTML code. 
+
+_Replace the below with your own design. Think of all the different possible responses your route will return._
+
+```
+200 OK 
+
+returns nothing else, just creates a new artist 
+
+```
+
+## 3. Write Examples
+
+_Replace these with your own design._
+
+```
+# Request:
+
+POST /artists?name:"Ariana Grande", genre:"Pop")
+
+# Expected response:
+
+Response for 200 OK
+
+
+```
+
+
+## 4. Encode as Tests Examples
+
+```ruby
+# EXAMPLE
+# file: spec/integration/application_spec.rb
+
+require "spec_helper"
+
+describe Application do
+  include Rack::Test::Methods
+
+  let(:app) { Application.new }
+
+  context "GET /" do
+    it 'returns 200 OK' do
+      # Assuming the post with id 1 exists.
+      response = get('/posts?id=1')
+
+      expect(response.status).to eq(200)
+      # expect(response.body).to eq(expected_response)
+    end
+
+    it 'returns 404 Not Found' do
+      response = get('/posts?id=276278')
+
+      expect(response.status).to eq(404)
+      # expect(response.body).to eq(expected_response)
+    end
+  end
+end
+```
+
+## 5. Implement the Route
+
+Write the route and web server code to implement the route behaviour.
+
+
+# {{ GET }} {{ /artists }} Route Design Recipe
+
+_Copy this design recipe template to test-drive a Sinatra route._
+
+## 1. Design the Route Signature
+
+You'll need to include:
+  * the HTTP method
+  * the path
+  * any query parameters (passed in the URL)
+  * or body parameters (passed in the request body)
+
+  METHOD: GET
+  PATH: /artists
+
+## 2. Design the Response
+
+The route might return different responses, depending on the result.
+
+For example, a route for a specific blog post (by its ID) might return `200 OK` if the post exists, but `404 Not Found` if the post is not found in the database.
+
+Your response might return plain text, JSON, or HTML code. 
+
+_Replace the below with your own design. Think of all the different possible responses your route will return._
+
+```
+Pixies
+ABBA
+Taylor Swift
+Nina Simone
+Kiasmos
+
+```
+
+## 3. Write Examples
+
+_Replace these with your own design._
+
+```
+# Request:
+
+GET /artists
+
+# Expected response:
+
+Response for 200 OK
+
+```
+Pixies, ABBA, Taylor Swift, Nina Simone, Kiasmos
+
+```
+
+## 4. Encode as Tests Examples
+
+```ruby 
+# EXAMPLE
+# file: spec/integration/application_spec.rb
+
+require "spec_helper"
+
+describe Application do
+  include Rack::Test::Methods
+
+  let(:app) { Application.new }
+
+  context "GET /" do
+    it 'returns 200 OK' do
+      # Assuming the post with id 1 exists.
+      response = get('/posts?id=1')
+
+      expect(response.status).to eq(200)
+      # expect(response.body).to eq(expected_response)
+    end
+
+    it 'returns 404 Not Found' do
+      response = get('/posts?id=276278')
+
+      expect(response.status).to eq(404)
+      # expect(response.body).to eq(expected_response)
+    end
+  end
+end
+```
+
+## 5. Implement the Route
+
+Write the route and web server code to implement the route behaviour.
+
+
+# {{ METHOD }} {{ PATH}} Route Design Recipe
+
+_Copy this design recipe template to test-drive a Sinatra route._
+
+## 1. Design the Route Signature
+
+You'll need to include:
+  * the HTTP method
+  * the path
+  * any query parameters (passed in the URL)
+  * or body parameters (passed in the request body)
+
+  Method: GET
+  Path: /albums
+
+
+## 2. Design the Response
+
+The route might return different responses, depending on the result.
+
+For example, a route for a specific blog post (by its ID) might return `200 OK` if the post exists, but `404 Not Found` if the post is not found in the database.
+
+Your response might return plain text, JSON, or HTML code. 
+
+_Replace the below with your own design. Think of all the different possible responses your route will return._
+
+````
+Surfer Rosa
+Waterloo
+Super Trouper
+Bossanova
+Lover
+Folklore
+I Put a Spell on You
+Baltimore
+Here Comes the Sun
+Fodder on My Wings
+Ring Ring
+````
+
+## 3. Write Examples
+
+_Replace these with your own design._
+
+```
+# Request:
+
+GET /albums
+
+# Expected response:
+
+Response for 200 OK
+
+Surfer Rosa
+Waterloo
+Super Trouper
+Bossanova
+Lover
+Folklore
+I Put a Spell on You
+Baltimore
+Here Comes the Sun
+Fodder on My Wings
+Ring Ring
+```
+
+
+## 4. Encode as Tests Examples
+
+```ruby
+# EXAMPLE
+# file: spec/integration/application_spec.rb
+
+require "spec_helper"
+
+describe Application do
+  include Rack::Test::Methods
+
+  let(:app) { Application.new }
+
+  context "GET /" do
+    it 'should return list of albums' do 
+    response = get('albums')
+
+    expected_response = 'Surfer Rosa, Waterloo, Super Trouper, Bossanova, Lover, Folklore, I Put a Spell on You, Baltimore, Here Comes the Sun, Fodder on My Wings, Ring Ring'
+
+    expect(response.status).to eq(200)
+    expect(response.body).to eq expected_response
+  end
+end
+```
+
+## 5. Implement the Route
+
+Write the route and web server code to implement the route behaviour.
+
+
+# file: app.rb
+require 'sinatra'
+require "sinatra/reloader"
+require_relative 'lib/database_connection'
+require_relative 'lib/album_repository'
+require_relative 'lib/artist_repository'
+
+DatabaseConnection.connect
+
+class Application < Sinatra::Base
+  configure :development do
+    register Sinatra::Reloader
+    also_reload 'lib/album_repository'
+    also_reload 'lib/artist_repository'
+  end
+
+  get '/albums' do 
+    repo = AlbumRepository.new
+    albums = repo.all 
+
+    response = albums.map do |album|
+      album.title
+    end.join(", ")
+
+    return response
+  end 
+
+  post '/albums' do 
+    repo = AlbumRepository.new
+    new_album = Album.new
+    new_album.title = params[:title]
+    new_album.release_year = params[:release_year]
+    new_album.artist_id = params[:artist_id]
+
+    repo.create(new_album)
+
+    return ''
+  end
+
+  get '/artists' do 
+    repo = ArtistRepository.new 
+
+    artists = repo.all
+
+    response = artists.map do |artist_name|
+      artist_name.name
+    end.join(", ")
+
+    return response
+  end 
+
+  post '/artists' do 
+    repo = ArtistRepository.new 
+
+    new_artist = Artist.new
+    new_artist.name = params[:name]
+    new_artist.genre = params[:name]
+
+    repo.create(new_artist)
+
+    return ''
+  end 
+end
